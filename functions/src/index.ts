9981bc06a53f665d22242df7efb5d0f0d220715b
Flavianthepavian <flavianzuellig@hotmail.com>
2024-11-05 16:30:08 +0100

    fix sign in procedure and checks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,11 @@
-import { onCall, onRequest } from "firebase-functions/v2/https";
+import { onCall } from "firebase-functions/v2/https";
+import { onDocumentUpdatedWithAuthContext } from "firebase-functions/v2/firestore";
 import { getFirestore } from "firebase-admin/firestore";
 import { initializeApp } from "firebase-admin/app";
-import { Answers, Game, GamePlayer, Guesses, Lobby } from "./types";
+import { Answers, Game, GamePlayer, Lobby } from "./types";
 import { v4 as generateUUID } from "uuid";
 import { getRandomSong } from "./utils";
-import {
-    BAD_REQUEST,
-    FORBIDDEN,
-    INTERNAL_ERROR,
-    OK,
-    UNAUTHORIZED,
-} from "./responses";
+import { BAD_REQUEST, FORBIDDEN, OK, UNAUTHORIZED } from "./responses";
 
 initializeApp();
 const firestore = getFirestore();
@@ -77,71 +72,80 @@ exports.startGame = onCall(async (request) => {
     return OK({ uuid: uuid });
 });
 
-exports.roundEnd = onRequest((request, response) => {
-    console.log("Received roundEnd", request);
-});
+exports.submitguess = onDocumentUpdatedWithAuthContext(
+    "/games/{gameDoc}/guesses/{guessDoc}/",
+    (event) => {
+        if (!event.data) {
+            console.error("Document updated did not contain any data");
+            return;
+        }
+        let after = event.data.after.data();
 
-exports.submitGuess = onCall(async (request) => {
-    if (!request.auth) {
-        return UNAUTHORIZED;
-    }
-
-    //ensure answers are in payload
-    if (
-        typeof request.data.code !== "string" ||
-        typeof request.data.answers !== "object"
-    ) {
-        return BAD_REQUEST;
-    }
-    if (
-        !(
-            typeof request.data.album === "string" &&
-            typeof request.data.title === "string" &&
-            typeof request.data.release === "number" &&
-            typeof request.data.author === "string"
-        )
-    ) {
-        return BAD_REQUEST;
-    }
-
-    let answers = request.data.answers as Answers;
-
-    // retrieve uuid
-    if (typeof request.data.uuid !== "string") {
-        return BAD_REQUEST;
-    }
-    let uuid: string = request.data.uuid;
-
-    let game_snapshot = await firestore.doc("/games/" + uuid).get();
-    if (!game_snapshot.exists) {
-        return BAD_REQUEST;
-    }
-
-    // ensure user is in game
-    let game = game_snapshot.data() as Game;
-    if (!Object.keys(game).includes(request.auth.uid)) {
-        return FORBIDDEN;
-    }
-
-    let guesses_snapshot = await firestore
-        .doc("/games/" + uuid + "/guesses/0")
-        .get();
-    if (!guesses_snapshot.exists) {
-        return INTERNAL_ERROR;
-    }
-    let guesses_doc = guesses_snapshot.data() as Guesses;
-
-    // check if user has already submitted a guess
-    if (request.auth.uid in guesses_doc) {
-        return FORBIDDEN;
-    }
-
-    let end = game.rounds[game.rounds.length - 1].end;
-    if (end + 300 < Date.now()) {
-        // time expired, with 300 ms buffer to avoid unexpected behavior
-        return FORBIDDEN;
-    }
-
-    if (end === 0) {
-    }
-});
+        console.log("after", Object.entries(after));
+    },
+);
+//
+// exports.submitGuess = onCall(async (request) => {
+//     if (!request.auth) {
+//         return UNAUTHORIZED;
+//     }
+//
+//     //ensure answers are in payload
+//     if (
+//         typeof request.data.code !== "string" ||
+//         typeof request.data.answers !== "object"
+//     ) {
+//         return BAD_REQUEST;
+//     }
+//     if (
+//         !(
+//             typeof request.data.album === "string" &&
+//             typeof request.data.title === "string" &&
+//             typeof request.data.release === "number" &&
+//             typeof request.data.author === "string"
+//         )
+//     ) {
+//         return BAD_REQUEST;
+//     }
+//
+//     let answers = request.data.answers as Answers;
+//
+//     // retrieve uuid
+//     if (typeof request.data.uuid !== "string") {
+//         return BAD_REQUEST;
+//     }
+//     let uuid: string = request.data.uuid;
+//
+//     let game_snapshot = await firestore.doc("/games/" + uuid).get();
+//     if (!game_snapshot.exists) {
+//         return BAD_REQUEST;
+//     }
+//
+//     // ensure user is in game
+//     let game = game_snapshot.data() as Game;
+//     if (!Object.keys(game).includes(request.auth.uid)) {
+//         return FORBIDDEN;
+//     }
+//
+//     let guesses_snapshot = await firestore
+//         .doc("/games/" + uuid + "/guesses/0")
+//         .get();
+//     if (!guesses_snapshot.exists) {
+//         return INTERNAL_ERROR;
+//     }
+//     let guesses_doc = guesses_snapshot.data() as Guesses;
+//
+//     // check if user has already submitted a guess
+//     if (request.auth.uid in guesses_doc) {
+//         return FORBIDDEN;
+//     }
+//
+//     let end = game.rounds[game.rounds.length - 1].end;
+//     if (end + 300 < Date.now()) {
+//         // time expired, with 300 ms buffer to avoid unexpected behavior
+//         return FORBIDDEN;
+//     }
+//
+//     if (end === 0) {
+//     }
+// });
